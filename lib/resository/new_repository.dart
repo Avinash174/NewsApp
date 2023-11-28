68ae529403b206617bf3e2d62197046eb0d853b7
Avinash174 <avinashmagar15@gmail.com>
2023-11-28 21:25:49 +0530

    new changes done

--- lib/resository/new_repository.dart
+++ lib/resository/new_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/model/new_channel_headline_model.dart';
 
@@ -8,6 +10,9 @@ class NewRepository {
     String url =
         "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=605863575875479abbca9bdd2c420208";
     final response = await http.get(Uri.parse(url));
+    if (kDebugMode) {
+      log(response.body);
+    }
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
 
--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:news_app/view_model/new_view_model.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,9 +13,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final format = DateFormat('MMMM dd, yyyy');
+  NewsViewModel newsViewModel = NewsViewModel();
   @override
   Widget build(BuildContext context) {
-    NewsViewModel newsViewModel = NewsViewModel();
     final height = MediaQuery.sizeOf(context).height * 1;
     final width = MediaQuery.sizeOf(context).width * 1;
     return Scaffold(
@@ -28,7 +32,8 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () {},
           icon: Image.asset(
             'assets/icons/category_icon.png',
-            height: height * .03,
+            height: 30,
+            width: 30,
           ),
         ),
         actions: [
@@ -40,6 +45,151 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: height * .55,
+            width: width,
+            child: FutureBuilder(
+                future: newsViewModel.fetchNewsChannelHeadlineApi(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: SpinKitCircle(
+                        color: Colors.blue,
+                        size: 50,
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.articles!.length,
+                      itemBuilder: (context, index) {
+                        DateTime dateTime = DateTime.parse(snapshot
+                            .data!.articles![index].publishedAt
+                            .toString());
+                        return SizedBox(
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Container(
+                                height: height * .6,
+                                width: width * .9,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: height * .02,
+                                ),
+                                child: ClipRRect(
+                                  borderRadius:
+                                      BorderRadiusDirectional.circular(
+                                    15.0,
+                                  ),
+                                  child: CachedNetworkImage(
+                                    imageUrl: snapshot
+                                        .data!.articles![index].urlToImage
+                                        .toString(),
+                                    fit: BoxFit.cover,
+                                    placeholder: (context, url) =>
+                                        Container(child: spinKit2),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(
+                                      Icons.error_outline_outlined,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 20,
+                                child: Card(
+                                  elevation: 5,
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      12,
+                                    ),
+                                  ),
+                                  child: Container(
+                                    height: height * .22,
+                                    padding: EdgeInsets.all(20),
+                                    alignment: Alignment.bottomCenter,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: width * .7,
+                                          child: Text(
+                                            snapshot
+                                                .data!.articles![index].title
+                                                .toString(),
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            textAlign: TextAlign.justify,
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w700,
+                                            ),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        Container(
+                                          child: SizedBox(
+                                            width: width * .7,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Text(
+                                                  snapshot
+                                                      .data!
+                                                      .articles![index]
+                                                      .source!
+                                                      .name
+                                                      .toString(),
+                                                  maxLines: 2,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: GoogleFonts.poppins(
+                                                    fontSize: 12,
+                                                    fontWeight: FontWeight.w600,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  format.format(dateTime),
+                                                  maxLines: 2,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: GoogleFonts.poppins(
+                                                    fontSize: 12,
+                                                    fontWeight: FontWeight.w600,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
+                }),
+          )
+        ],
+      ),
     );
   }
 }
+
+const spinKit2 = SpinKitFadingCircle(
+  color: Colors.amber,
+  size: 40,
+);
