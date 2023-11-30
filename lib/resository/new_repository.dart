414bd6bf6f42f4c37811e593ac9fc2023b2da20d
Avinash174 <avinashmagar15@gmail.com>
2023-11-30 16:02:01 +0530

    new changes done

--- lib/resository/new_repository.dart
+++ lib/resository/new_repository.dart
@@ -9,7 +9,7 @@ class NewRepository {
   Future<NewChannelHeadlineModel> fetchNewsChannelHeadlineApi(
       String chnannelName) async {
     String url =
-        "https://newsapi.org/v2/top-headlines?sources=${chnannelName}&apiKey=605863575875479abbca9bdd2c420208";
+        "https://newsapi.org/v2/top-headlines?sources=${chnannelName}&apiKey=092b8ab9a0e44c67b875c9a0d74a2852";
     final response = await http.get(Uri.parse(url));
     if (kDebugMode) {
       log(response.body);
@@ -24,7 +24,7 @@ class NewRepository {
 
   Future<NewChannelHeadlineModel> fetchNewsCategories(String category) async {
     String url =
-        "https://newsapi.org/v2/everything?q=${category}&apiKey=605863575875479abbca9bdd2c420208";
+        "https://newsapi.org/v2/everything?q=${category}&apiKey=092b8ab9a0e44c67b875c9a0d74a2852";
     final response = await http.get(Uri.parse(url));
     if (kDebugMode) {
       log(response.body);
