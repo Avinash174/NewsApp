761f9c997f77007d14af6b2978b550e433beb380
Avinash174 <avinashmagar15@gmail.com>
2023-11-30 13:49:01 +0530

    new changes done

--- lib/view/categories_screen.dart
+++ lib/view/categories_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:news_app/view_model/new_view_model.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
@@ -8,14 +11,67 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  final format = DateFormat('MMMM dd, yyyy');
+  String categoryName = 'General';
+  NewsViewModel newsViewModel = NewsViewModel();
+
+  List<String> catgoriesList = [
+    'General',
+    'Entertainment',
+    'Health',
+    'Sports',
+    'Business',
+    'Technology',
+  ];
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height * 1;
     final width = MediaQuery.sizeOf(context).width * 1;
     return Scaffold(
       appBar: AppBar(),
-      body: const Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: height * .05,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: catgoriesList.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        categoryName = catgoriesList[index];
+                        setState(() {});
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 12.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: categoryName == catgoriesList[index]
+                                  ? Colors.blue
+                                  : Colors.grey,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: Center(
+                              child: Text(
+                                catgoriesList[index].toString(),
+                                style: GoogleFonts.poppins(
+                                  fontSize: 13,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
